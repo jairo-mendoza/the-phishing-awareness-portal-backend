6184c97e43969357ed939f042366303ba9eebdf3
jairo-mendoza <jairomendoza629@gmail.com>
2024-04-04 10:20:52 -0700

    Create the user model

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+// Defining structure of user mongodb document
+const userSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastName: {
+            type: String,
+            required: true,
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+// Hash user password before user document is saved to database
+userSchema.pre("save", async function (next) {
+    // Mongoose Document object functions:
+    // https://mongoosejs.com/docs/api/document.html#Document.prototype.$isModified()
+    // Only hash the password if it is modified or a new user document
+    if (this.isModified("password") || this.isNew) {
+        const hash = await bcrypt.hash(this.password, saltRounds);
+        this.password = hash;
+    }
+
+    next();
+});
+
+// Method to compare entered password with database password for logins
+userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
